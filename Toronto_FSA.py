db3e8bdf34e0201a9bd60d2c5139662b29ceee96
Thomas Adams 3 <thomasadams@Jessicas-MacBook-Pro.local>
2019-07-25 11:44:19 -0400

    added in lat lng to dataframe

--- Toronto_FSA.py
+++ Toronto_FSA.py
@@ -1,30 +1,68 @@
-
+# library to handle requests
+import requests
+# library for data analsysis
 import pandas as pd
+# library
 import wikipedia as wp
+import numpy as np
+import folium
+import json
+
+# Matplotlib and associated plotting modules
+import matplotlib.cm as cm
+import matplotlib.colors as colors
 
 
-# More than one neighborhood can exist in one postal code area. For example, in the table on the Wikipedia page, you will notice that M5A is listed twice and has two neighborhoods: Harbourfront and Regent Park. These two rows will be combined into one row with the neighborhoods separated with a comma as shown in row 11 in the above table.
-# If a cell has a borough but a Not assigned neighborhood, then the neighborhood will be the same as the borough. So for the 9th cell in the table on the Wikipedia page, the value of the Borough and the Neighborhood columns will be Queen's Park.
-# Clean your Notebook and add Markdown cells to explain your work and any assumptions you are making.
-# In the last cell of your notebook, use the .shape method to print the number of rows of your dataframe.
+from geopy.geocoders import Nominatim
 
+## First section of my program: Scrape, clean, organize data into dataFrame.
+## Thomas Adams
 
 # grabbed the table from wiki and saved to a df & csv
 html = wp.page("List_of_postal_codes_of_Canada:_M").html().encode("UTF-8")
 Toronto_FSA = pd.read_html(html)[0]
 Toronto_FSA.to_csv('beautifulsoup_toronto.csv',header=0,index=False)
 
+## First section of my program: Scrape, clean, organize data into dataFrame.
 # filter out Not assigned entries
 filter_tor1 = Toronto_FSA[Toronto_FSA['Borough'] != 'Not assigned'].reset_index(drop=True)
+
 # if postcode is same for any rows. Combine the two rows with the neighborhoods separated with a comma
-#filter_tor1.groupby('Postcode').agg({'Neighbourhood': 'sum', '': lambda x: ' '.join(x)})
 filter_tor2 = filter_tor1.groupby('Postcode')['Neighbourhood'].unique().agg(', '.join)
 filter_tor2v = filter_tor1.groupby('Postcode')['Borough'].unique().agg(', '.join)
 toronto_final = pd.concat([filter_tor2v, filter_tor2], axis=1).reset_index()
 
 
+
+
+
+
+
+# =============================================================================
+# import geocoder
+# 
+# def get_geocoder(postal_code):
+#      # initialize your variable to None
+#      lat_lng_coords = None
+#      # loop until you get the coordinates
+#      while(lat_lng_coords is None):
+#        g = geocoder.google('{}, Toronto, Ontario'.format(postal_code))
+#        lat_lng_coords = g.latlng
+#      latitude = lat_lng_coords[0]
+#      longitude = lat_lng_coords[1]
+#      return latitude,longitude
+# 
+# 
+# toronto_final['Latitude'], toronto_final['Longitude'] = zip(*toronto_final['Postcode'].apply(get_geocoder))  
+# =============================================================================
+
+# grabs the latlng of all the postal codes in Toronto
+toronto_latlng = pd.read_csv('https://cocl.us/Geospatial_data')
+
+# adds the lat lng values to the toronto_final dataFrame
+toronto_final['Latitude'] = toronto_latlng['Latitude']
+toronto_final['Longitude'] = toronto_latlng['Longitude']
+
+
 print(toronto_final.shape)
 
-
-
-    
